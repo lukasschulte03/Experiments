5172438d41f5159b21d24ee1ad0c83861dce4f80
lukasschulte03 <lukas007@live.se>
2022-03-15 15:43:21 +0100

    WIP

--- RPS comparison test.py
+++ RPS comparison test.py
@@ -22,11 +22,11 @@ def winner(p1, p2):
 while True:
     player = random.randint(1,3)
     opponent = random.randint(1,3)
-    print("player chose: " + dic[player])
+    print("\n---------------------------------------------")
+    print("\nplayer chose: " + dic[player])
     print("opponent chose: " + dic[opponent])
     print(str(winner(player - 1, opponent - 1)))
-    print(" \n ")
 
-    answer = input("Play again? y/n: ") 
+    answer = input("\nPlay again? y/n: ") 
     if answer != "y":
         break
